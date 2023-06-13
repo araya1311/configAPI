b42364b2a15b89e05b43a23bae8d05f5fc679d61
araya1311 <arayavidel@gmail.com>
2023-06-13 15:31:44 -0600

    creacion de services

---
+++ configAPI/Controllers/ConfigController.cs
@@ -0,0 +1,17 @@
+﻿using configAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace configAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Config> GetConfig()
+        {
+            return new List<Config>();
+        }
+    }
+}
---
+++ configAPI/Models/Config.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace configAPI.Models
+{
+    public class Config
+    {
+        public int Id { get; set; }
+        public int CountryId { get; set; }
+        public bool FinalCustomer { get; set; }
+        public bool InvoicingConcept { get; set; }
+        public bool InvoicingPeriod { get; set; }
+        public bool InvoicingStart { get; set; }
+        public bool TaxBase { get; set; }
+        public bool Iva { get; set; }
+        public bool TotalAmount { get; set; }
+        public string Language { get; set; }
+        public string SenderMail { get; set; }
+        public byte[] LogoUfinet { get; set; }
+
+    }
+}
